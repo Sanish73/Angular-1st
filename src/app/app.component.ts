5b88adc35d52905727546a40787be16ff779ab3b
Sanish Thapa <thapasanish73@gmail.com>
2022-12-15 18:48:41 +0545

    sdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'WebApplicationTraining';
+  name = '.............';
   constructor(){
     setTimeout(() => {
       this.name = "Sanish Thapa"
