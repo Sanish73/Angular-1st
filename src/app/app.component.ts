8ebf60fdcf81cb9b0e6dcbe4fc8efddb1616e7aa
Sanish Thapa <thapasanish73@gmail.com>
2022-12-21 14:27:17 +0545

    dfds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+
+ 
+
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
-  title = '.............';
-  // constructor(){
-  //   setTimeout(() => {
-  //     this.name = "Sanish Thapa"
-  //   }, 2000);
-  // }
+  title = 'Sanish';
+  constructor(){
+    setTimeout(() => {
+      this.title = "Home"
+    }, 2000);
+  }
 }
